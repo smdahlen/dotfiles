836eb40aa6340d517f000115647c572f1e7827ef
Shawn Dahlen <shawn@dahlen.me>
2025-07-29 07:11:41 -0400

    combine lspconfig and mason

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,28 @@
 return {
   {
+    'neovim/nvim-lspconfig',
+  },
+  {
     'williamboman/mason.nvim',
     opts = {},
   },
   {
     'williamboman/mason-lspconfig.nvim',
-    opts = {
-      ensure_installed = { 'lua_ls' },
-    },
     dependencies = {
       'williamboman/mason.nvim',
       'neovim/nvim-lspconfig',
     },
+    opts = {
+      ensure_installed = { 'lua_ls', 'pyright' },
+    },
   },
   {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-    opts = {
-      ensure_installed = { 'stylua' },
-    },
     dependencies = {
       'williamboman/mason.nvim',
+    },
+    opts = {
+      ensure_installed = { 'stylua', 'ruff' },
     },
   },
 }
